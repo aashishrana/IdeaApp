cc968fb15419674dfed95e62ce0f3062f2abd384
aashishrana <aashishrana71135@gmail.com>
2023-07-14 00:23:19 -0700

    Now finally error of database is solved

--- configs/db.config.js
+++ configs/db.config.js
@@ -1,4 +1,7 @@
+// localhost : 0.0.0.0;
+
 module.exports = {
     DB_NAME : "idea_app_demo",
-    DB_URL : "mongodb://localhost/idea_app_demo",
+    
+    DB_URL : "mongodb://0.0.0.0:27017/idea_app_demo",
 }
